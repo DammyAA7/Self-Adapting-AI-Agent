c11de9154bd3d8e6e5c863b8ff3dadbc6ed98fa6
AIFahim <fahim.iqbal@infinitibit.com>
2025-09-29 04:56:27 +0600

    Add Movie API task logs and dataset

--- framework_our_approach/ablation_study_humaneval_and_long_horizon_tasks/functions.py
+++ framework_our_approach/ablation_study_humaneval_and_long_horizon_tasks/functions.py
@@ -1,23 +1,5 @@
 # Dynamically generated functions will be added here
 
-
-
-def count_up_to(n):
-    '''Returns a list of prime numbers less than n.
-    Raises TypeError for non-integer inputs.'''
-    if not isinstance(n, int):
-        raise TypeError("Input must be an integer")
-    if n <= 2:
-        return []
-    sieve = [True] * n
-    sieve[0:2] = [False, False]
-    for i in range(2, int(n**0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n, i):
-                sieve[j] = False
-    return [i for i, is_prime in enumerate(sieve) if is_prime]
-
-
 if __name__ == "__main__":
     pass
 
--- framework_w_o_tdd/ablation_study_humaneval_and_long_horizon_tasks/functions.py
+++ framework_w_o_tdd/ablation_study_humaneval_and_long_horizon_tasks/functions.py
@@ -1,32 +1,5 @@
 # Dynamically generated functions will be added here
 
-
-
-def split_words(txt):
-    """
-    Splits the input text into a list according to the following rules:
-    - If the text contains any whitespace characters, split on whitespace and return the list of resulting words.
-    - Otherwise, if the text contains commas, split on commas and return the list of resulting segments.
-    - If neither whitespace nor commas are present, count and return the number of lowercase letters whose alphabetical index is odd (where ord('a')=0, ord('b')=1, …, ord('z')=25).
-
-    Parameters:
-    txt (string): The input string to process.
-
-    Returns:
-    List[str] or int: Result based on the splitting/counting rules.
-    """
-    if any(c.isspace() for c in txt):
-        return txt.split()
-    elif ',' in txt:
-        return txt.split(',')
-    else:
-        count = 0
-        for c in txt:
-            if 'a' <= c <= 'z' and (ord(c) - ord('a')) % 2 == 1:
-                count += 1
-        return count
-
-
 if __name__ == "__main__":
     pass
 

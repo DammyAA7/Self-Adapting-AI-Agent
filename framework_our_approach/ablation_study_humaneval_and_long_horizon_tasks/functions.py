a1ffe195b1782ce949902f2b2b262213c05c5c36
AIFahim <fahim.iqbal@infinitibit.com>
2025-10-09 17:22:04 +0600

    Update o4-mini model changed

--- framework_our_approach/ablation_study_humaneval_and_long_horizon_tasks/functions.py
+++ framework_our_approach/ablation_study_humaneval_and_long_horizon_tasks/functions.py
@@ -1,7 +1,28 @@
 # Dynamically generated functions will be added here
 
-if __name__ == "__main__":
-    pass
+computation_cache = {}
+
+
+
+def filter_by_substring(strings, substring):
+    """Filters a list of strings, returning those that contain the given substring.
+
+    Args:
+        strings (list): List of strings to filter.
+        substring (str): Substring to search for.
+
+    Returns:
+        list: List of strings containing the substring.
+
+    Raises:
+        TypeError: If 'strings' is not a list or 'substring' is not a string.
+    """
+    if not isinstance(strings, list):
+        raise TypeError("'strings' must be a list")
+    if not isinstance(substring, str):
+        raise TypeError("'substring' must be a string")
+    return [s for s in strings if substring in s]
+
 
 if __name__ == "__main__":
     pass
